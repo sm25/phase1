3f61bd6868a8bd9e4d5d83cac1370b76c3355d32
sm25 <smukesh3@gatech.edu>
2014-09-28 17:45:57 -0400

    Update Phase1.m

--- Phase1.m
+++ Phase1.m
@@ -1,14 +1,14 @@
 %%Hodgkin-Huxley
-t=0:0.00025:0.1;    %Define time
+t=0:0.0001:0.1;    %Define time
 gK=0.036;           %Initial conductance for K
 gNa=0.12;           %Initial conductance for Na
 gL=0.003;           %Initial conductance for the leakage current
-E_K=(-1*0.012);     %Initial potential for K channels
+E_K=(-0.012);     %Initial potential for K channels
 E_Na=0.115;         %Initial potential for Na channels
 E_L=0.0106;         %Initial potential associated with leakage
-V_rest=(-1*0.07);   %Resting potential
+V_rest=(-0.07);   %Resting potential
 C_m=1*(10^(-6));    %Membrane capacitance
-frac=0.00025;       %factor by which time is incrementing
+frac=0.0001;       %factor by which time is incrementing
 
 %%Initializing vectors!!
 
@@ -16,6 +16,7 @@ frac=0.00025;       %factor by which time is incrementing
 V_new=zeros([1 length(t)]);     
 V=zeros([1 length(t)]);
 V(1)=V_rest;
+V_new(1)=V_rest;
 %Initialize the gating variables
 alpha_m=zeros([1 length(t)]);
 alpha_n=zeros([1 length(t)]);
@@ -28,29 +29,26 @@ m=zeros([1 length(t)]);
 n=zeros([1 length(t)]);
 h=zeros([1 length(t)]);
 %Initialize the various current vectors
-I_Na=zeros([1 length(t)]);
-I_K=zeros([1 length(t)]);
-I_L=zeros([1 length(t)]);
+% % I_Na=zeros([1 length(t)]);
+% % I_K=zeros([1 length(t)]);
+% % I_L=zeros([1 length(t)]);
 I_ion=zeros([1 length(t)]);
 
 
 %%%%%Trying to provide stumulus
 I_inj=zeros([1 length(t)]);
-% % I_inj_1=ones([1 20]); % Specify the duration of the stimulus in terms of the no. of samples 
-% % a=length(I_inj_1);
-% % I_inj(1, 10:(9+a))=((5*10^(-6))*I_inj_1);
+I_inj_1=ones([1 50]); % Specify the duration of the stimulus in terms of the no. of samples 
+a=length(I_inj_1);
+I_inj(1, 10:(9+a))=((5*10^(-6))*I_inj_1);
 
+alpha_m(1)= 0.1*((25-V(1))/exp((25-V(1))/10)-1);
+beta_m(1)=4.*exp(((-1)*V(1))/18);
+alpha_n(1)=0.01*((10-V(1))/(exp((10-V(1))/10)-1));
+beta_n(1)=0.0125*exp(-V(1)/80);
+alpha_h(1)=0.07*exp(-V(1)/20);
+beta_h(1)=1./(exp((30-V(1))/10)+1);
 
-for i=1:(length(t)-1)
-    
-%Calculate values for the gating variables
-alpha_m(i)= 0.1*((25-V(i))/exp((25-V(i))/10)-1);
-beta_m(i)=4.*exp(((-1)*V(i))/18);
-alpha_n(i)=0.01*((10-V(i))/(exp((10-V(i))/10)-1));
-beta_n(i)=0.0125*exp(-V(i)/80);
-alpha_h(i)=0.07*exp(-V(i)/20);
-beta_h(i)=1./(exp((30-V(i))/10)+1);
-
+I_ion(1)=0;
 
 %Calculate values for the conductance related variables
 
@@ -59,27 +57,31 @@ n(1) = alpha_n(1)./(alpha_n(1)+beta_n(1));
 h(1) = alpha_h(1)./(alpha_h(1)+beta_h(1));
 
 
+for i=2:(length(t)-1)
+ 
+ V(i)=V_new(i-1);
+%Calculate values for the gating variables
+alpha_m(i)= 0.1*((25-V(i))/exp((25-V(i))/10)-1);
+beta_m(i)=4.*exp(((-1)*V(i))/18);
+alpha_n(i)=0.01*((10-V(i))/(exp((10-V(i))/10)-1));
+beta_n(i)=0.0125*exp(-V(i)/80);
+alpha_h(i)=0.07*exp(-V(i)/20);
+beta_h(i)=1./(exp((30-V(i))/10)+1);
+
 %Update the variables to the next value using Euler's method
 
-m(i+1) = m(i) + frac.*(alpha_m(i).*(1-m(i)) - (beta_m(i).*m(i)));
-n(i+1) = n(i) + frac.*(alpha_n(i).*(1-n(i)) - (beta_n(i).*n(i)));
-h(i+1) = h(i) + frac.*(alpha_h(i).*(1-h(i)) - (beta_h(i).*h(i)));
+m(i) = m(i-1) + frac.*(alpha_m(i-1).*(1-m(i-1)) - (beta_m(i-1).*m(i-1)));
+n(i) = n(i-1) + frac.*(alpha_n(i-1).*(1-n(i-1)) - (beta_n(i-1).*n(i-1)));
+h(i) = h(i-1) + frac.*(alpha_h(i-1).*(1-h(i-1)) - (beta_h(i-1).*h(i-1)));
 
 % Currents, calculate the current values
 
-I_Na(i)=(m(i).^3)*gNa.*h(i).*(V(i)-E_Na);
-I_K(i)=(n(i).^4)*gK.*(V(i)-E_K);
-I_L(i)=gL.*(V(i)-E_L);
-I_ion(i)=I_inj(i)-(I_Na(i))-(I_K(i))-(I_L(i));
+I_Na=(m(i).^3)*gNa.*h(i).*(V(i-1)-E_Na);
+I_K=(n(i).^4)*gK.*(V(i-1)-E_K);
+I_L=gL.*(V(i-1)-E_L);
+I_ion(i)=I_inj(i)-(I_Na)-(I_K)-(I_L);
 
 %Update the Voltage to the next value using Euler's method
 
-V_new(i)=V(i)+(frac.*(I_ion(i)/C_m));
-V(i+1)=V_new(i);
+V_new(i)=V(i-1)+(frac*(I_ion(i-1)./C_m));
 end
-
-% plot(t,V_rest,'r-');subplot(3,1,1);plot(t,gK,'r-');subplot(3,1,2);plot(t,gNa,'b-');subplot(3,1,3);
-% plot(t,V(1:length(t)),'r-');
-% plot(t,n,'b-');
-% plot(t,m,'g-');
-
